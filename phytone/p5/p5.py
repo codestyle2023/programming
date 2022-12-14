026867fcdc41649da9a265fd6c21521d8028fb29
mosi <83770847+MostafaEsl@users.noreply.github.com>
2022-12-14 21:03:43 +0330

    Add files via upload

---
+++ phytone/p5/p5.py
@@ -0,0 +1,3 @@
+x=(input("Enter charecter:"))
+for i in x:
+    print("ascii code is:\n",ord(x))
---
+++ phytone/p9/p9.py
@@ -0,0 +1,9 @@
+a=float(input("addad aval:"))
+b=float(input("addad dovom:"))
+c=float(input("addad sevom:"))
+if (a>b)and(a>c):
+    print(a,"is the biggest num")
+if (b>a)and(b>c):
+    print(b,"is the biggest num")
+if (c>b)and(c>a):
+    print(c,"is the biggest num")
