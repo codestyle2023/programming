c4a9755bd57e59b9d450cc44bb3d327a6d051e8d
arghavan8003 <111639308+arghavan8003@users.noreply.github.com>
2022-12-17 07:19:27 -0800

    added practice 8

---
+++ cpp/p8/p8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+   {
+	double a, b, c, m;
+	cin >> a >> b >> c; 
+	if(a*a == b*b + c*c)  
+	m = b * c / 2; 
+	else 
+	if(b*b == a*a + c*c) 
+	 m = a * c / 2; 
+	 else 
+	if(c*c == a*a + b*b)  
+	 m = a * b / 2;
+	else  
+	m = a + b + c; 										             
+	cout << m << endl;
+ } 	
