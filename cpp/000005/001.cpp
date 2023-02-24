cb178518fed99fd2dfaadf346371e822ee149290
Mostafa Eslami <83770847+MostafaEsl@users.noreply.github.com>
2023-02-24 17:32:34 +0330

    Add files via upload

---
+++ cpp/000005/001.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+main()
+{
+ char x;
+ cin >> x;
+ cout << x+1-1 << endl;
+}
---
+++ cpp/000007/001.cpp
@@ -0,0 +1,14 @@
+	#include<iostream>
+	 using namespace std;
+	  main() 
+	  {    
+	 double a, b, c;     
+	cin >> a >> b >> c;    
+	 if(a == b && a == c)        
+	 cout << "motasviol azla" << endl;    
+	else      
+	 if(a == b || a == c || b == c)           
+	 cout << "motasaviossagheyn" << endl;     
+	  else        
+ cout << "mokhtalefol azla" << endl; 
+						}
